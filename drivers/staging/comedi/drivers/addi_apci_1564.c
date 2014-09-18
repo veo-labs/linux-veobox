aaaac52e96c19231080f108cb377f36e88b91f0a
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-09-18 11:35:20 -0700

    staging: comedi: addi_apci_1564: use comedi_handle_events()
    Use comedi_handle_events() to automatically (*cancel) the async command
    for an end-of-acquisition or if an error/overflow occurs.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi_apci_1564.c
+++ drivers/staging/comedi/drivers/addi_apci_1564.c
@@ -166,9 +166,10 @@ static irqreturn_t apci1564_interrupt(int irq, void *d)
 		outl(status & APCI1564_DI_INT_DISABLE,
 		     dev->iobase + APCI1564_DI_IRQ_REG);
 
-		s->state = inl(dev->iobase + APCI1564_DI_INT_STATUS_REG) &
-			   0xffff;
-		comedi_buf_write_samples(s, &s->state, 1);
+		s->state = inl(dev->iobase + APCI1564_DI_INT_STATUS_REG)
+			       & 0xffff;
+		comedi_buf_put(s, s->state);
+		s->async->events |= COMEDI_CB_BLOCK | COMEDI_CB_EOS;
 		comedi_handle_events(dev, s);
 
 		/* enable the interrupt */
