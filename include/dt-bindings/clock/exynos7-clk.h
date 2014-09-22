7440121b59ac47ba3d6e924695bff04c30bb3239
Naveen Krishna Ch <naveenkrishna.ch@gmail.com>
2014-09-22 10:17:04 +0530

    clk: samsung: add initial clock support for Exynos7 SoC
    Add initial clock support for Exynos7 SoC which is required
    to bring up platforms based on Exynos7.
    
    Signed-off-by: Naveen Krishna Ch <naveenkrishna.ch@gmail.com>
    Signed-off-by: Abhilash Kesavan <a.kesavan@samsung.com>
    Reviewed-by: Thomas Abraham <thomas.ab@samsung.com>
    Tested-by: Thomas Abraham <thomas.ab@samsung.com>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>

--- include/dt-bindings/clock/exynos7-clk.h
+++ include/dt-bindings/clock/exynos7-clk.h
@@ -16,12 +16,7 @@
 #define DOUT_SCLK_BUS1_PLL		3
 #define DOUT_SCLK_CC_PLL		4
 #define DOUT_SCLK_MFC_PLL		5
-#define DOUT_ACLK_CCORE_133		6
-#define DOUT_ACLK_MSCL_532		7
-#define ACLK_MSCL_532			8
-#define DOUT_SCLK_AUD_PLL		9
-#define FOUT_AUD_PLL			10
-#define TOPC_NR_CLK			11
+#define TOPC_NR_CLK			6
 
 /* TOP0 */
 #define DOUT_ACLK_PERIC1		1
@@ -40,35 +35,10 @@
 #define CLK_SCLK_I2S1			14
 #define TOP0_NR_CLK			15
 
-/* TOP1 */
-#define DOUT_ACLK_FSYS1_200		1
-#define DOUT_ACLK_FSYS0_200		2
-#define DOUT_SCLK_MMC2			3
-#define DOUT_SCLK_MMC1			4
-#define DOUT_SCLK_MMC0			5
-#define CLK_SCLK_MMC2			6
-#define CLK_SCLK_MMC1			7
-#define CLK_SCLK_MMC0			8
-#define TOP1_NR_CLK			9
-
-/* CCORE */
-#define PCLK_RTC			1
-#define CCORE_NR_CLK			2
-
 /* PERIC0 */
 #define PCLK_UART0			1
 #define SCLK_UART0			2
-#define PCLK_HSI2C0			3
-#define PCLK_HSI2C1			4
-#define PCLK_HSI2C4			5
-#define PCLK_HSI2C5			6
-#define PCLK_HSI2C9			7
-#define PCLK_HSI2C10			8
-#define PCLK_HSI2C11			9
-#define PCLK_PWM			10
-#define SCLK_PWM			11
-#define PCLK_ADCIF			12
-#define PERIC0_NR_CLK			13
+#define PERIC0_NR_CLK			3
 
 /* PERIC1 */
 #define PCLK_UART1			1
@@ -77,54 +47,12 @@
 #define SCLK_UART1			4
 #define SCLK_UART2			5
 #define SCLK_UART3			6
-#define PCLK_HSI2C2			7
-#define PCLK_HSI2C3			8
-#define PCLK_HSI2C6			9
-#define PCLK_HSI2C7			10
-#define PCLK_HSI2C8			11
-#define PCLK_SPI0			12
-#define PCLK_SPI1			13
-#define PCLK_SPI2			14
-#define PCLK_SPI3			15
-#define PCLK_SPI4			16
-#define SCLK_SPI0			17
-#define SCLK_SPI1			18
-#define SCLK_SPI2			19
-#define SCLK_SPI3			20
-#define SCLK_SPI4			21
-#define PCLK_I2S1			22
-#define PCLK_PCM1			23
-#define PCLK_SPDIF			24
-#define SCLK_I2S1			25
-#define SCLK_PCM1			26
-#define SCLK_SPDIF			27
-#define PERIC1_NR_CLK			28
+#define PERIC1_NR_CLK			7
 
 /* PERIS */
 #define PCLK_CHIPID			1
 #define SCLK_CHIPID			2
-#define PCLK_WDT			3
-#define PCLK_TMU			4
-#define SCLK_TMU			5
-#define PERIS_NR_CLK			6
-
-/* FSYS0 */
-#define ACLK_MMC2			1
-#define ACLK_AXIUS_USBDRD30X_FSYS0X	2
-#define ACLK_USBDRD300			3
-#define SCLK_USBDRD300_SUSPENDCLK	4
-#define SCLK_USBDRD300_REFCLK		5
-#define PHYCLK_USBDRD300_UDRD30_PIPE_PCLK_USER		6
-#define PHYCLK_USBDRD300_UDRD30_PHYCLK_USER		7
-#define OSCCLK_PHY_CLKOUT_USB30_PHY		8
-#define ACLK_PDMA0			9
-#define ACLK_PDMA1			10
-#define FSYS0_NR_CLK			11
-
-/* FSYS1 */
-#define ACLK_MMC1			1
-#define ACLK_MMC0			2
-#define FSYS1_NR_CLK			3
+#define PERIS_NR_CLK			3
 
 /* MSCL */
 #define USERMUX_ACLK_MSCL_532		1
