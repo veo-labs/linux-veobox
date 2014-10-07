f0c3b95ce8f8bef7b484634dc4fb785b63a0ee5f
Heena Sirwani <heenasirwani@gmail.com>
2014-10-07 17:29:11 +0530

    Staging: lustre: lustre: osc: Changed the return code of a function from int to void.
    The following patch changes the return code of a function from int to void
    as the return value was not used by the callers.
    
    Signed-off-by: Heena Sirwani <heenasirwani@gmail.com>
    Reviewed-by: Octavian Purdila <octavian.purdila@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/osc/osc_cache.c
+++ drivers/staging/lustre/lustre/osc/osc_cache.c
@@ -607,7 +607,6 @@ void osc_extent_release(const struct lu_env *env, struct osc_extent *ext)
 		osc_io_unplug_async(env, osc_cli(obj), obj);
 	}
 	osc_extent_put(env, ext);
-	return 0;
 }
 
 static inline int overlapped(struct osc_extent *ex1, struct osc_extent *ex2)
