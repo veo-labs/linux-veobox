8c2adfbddebcb77c139336769300e98a71a8f167
Dzmitry Sledneu <dzmitry.sledneu@gmail.com>
2014-10-09 09:22:43 +0200

    staging: gs_fpgaboot: Fix "out of memory" error handling
    Fix "out of memory" error handling
    
    Signed-off-by: Dzmitry Sledneu <dzmitry.sledneu@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gs_fpgaboot/gs_fpgaboot.c
+++ drivers/staging/gs_fpgaboot/gs_fpgaboot.c
@@ -293,8 +293,8 @@ static int gs_fpgaboot(void)
 	struct fpgaimage	*fimage;
 
 	fimage = kmalloc(sizeof(struct fpgaimage), GFP_KERNEL);
-	if (fimage == NULL)
-		goto err_out;
+	if (!fimage)
+		return -ENOMEM;
 
 	err = gs_load_image(fimage, file);
 	if (err) {
