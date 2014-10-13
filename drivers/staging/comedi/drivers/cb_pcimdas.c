ffc96665ed8007424719b9bd5be9836be51f5fe1
Ian Abbott <abbotti@mev.co.uk>
2014-10-13 13:41:20 +0100

    staging: comedi: cb_pcimdas: use preferred block comment style
    Use the preferred block comment style for the copyright and Comedi
    driver description header comments.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/cb_pcimdas.c
+++ drivers/staging/comedi/drivers/cb_pcimdas.c
@@ -1,6 +1,6 @@
 /*
  * comedi/drivers/cb_pcimdas.c
- * Comedi driver for Computer Boards PCIM-DAS1602/16 and PCIe-DAS1602/16
+ * Comedi driver for Computer Boards PCIM-DAS1602/16
  *
  * COMEDI - Linux Control and Measurement Device Interface
  * Copyright (C) 2000 David A. Schleef <ds@schleef.org>
@@ -19,26 +19,22 @@
 /*
  * Driver: cb_pcimdas
  * Description: Measurement Computing PCI Migration series boards
- * Devices: [ComputerBoards] PCIM-DAS1602/16 (cb_pcimdas), PCIe-DAS1602/16
+ * Devices: [ComputerBoards] PCIM-DAS1602/16 (cb_pcimdas)
  * Author: Richard Bytheway
- * Updated: Mon, 13 Oct 2014 11:57:39 +0000
+ * Updated: Wed, 13 Nov 2002 12:34:56 +0000
  * Status: experimental
  *
- * Written to support the PCIM-DAS1602/16 and PCIe-DAS1602/16.
+ * Written to support the PCIM-DAS1602/16 on a 2.4 series kernel.
  *
  * Configuration Options:
- *   none
- *
- * Manual configuration of PCI(e) cards is not supported; they are configured
- * automatically.
+ *     [0] - PCI bus number
+ *     [1] - PCI slot number
  *
  * Developed from cb_pcidas and skel by Richard Bytheway (mocelet@sucs.org).
  * Only supports DIO, AO and simple AI in it's present form.
  * No interrupts, multi channel or FIFO AI,
  * although the card looks like it could support this.
- *
- * http://www.mccdaq.com/PDFs/Manuals/pcim-das1602-16.pdf
- * http://www.mccdaq.com/PDFs/Manuals/pcie-das1602-16.pdf
+ * See http://www.mccdaq.com/PDFs/Manuals/pcim-das1602-16.pdf for more details.
  */
 
 #include <linux/module.h>
