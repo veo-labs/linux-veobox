ea8a21d415d722631b364ddb4f228bf33d39c61c
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-10-22 14:36:35 -0700

    staging: comedi: amplc_pci224: use comedi_buf_read_samples()
    Use the new generic method to read the analog output samples from the async
    buffer.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_pci224.c
+++ drivers/staging/comedi/drivers/amplc_pci224.c
@@ -513,11 +513,18 @@ static void pci224_ao_handle_fifo(struct comedi_device *dev,
 {
 	struct pci224_private *devpriv = dev->private;
 	struct comedi_cmd *cmd = &s->async->cmd;
-	unsigned int bytes_per_scan = comedi_bytes_per_scan(s);
 	unsigned int num_scans;
 	unsigned int room;
 	unsigned short dacstat;
 	unsigned int i, n;
+
+	/* Determine number of scans available in buffer. */
+	num_scans = comedi_buf_read_n_available(s) / comedi_bytes_per_scan(s);
+	if (cmd->stop_src == TRIG_COUNT) {
+		/* Fixed number of scans. */
+		if (num_scans > devpriv->ao_stop_count)
+			num_scans = devpriv->ao_stop_count;
+	}
 
 	/* Determine how much room is in the FIFO (in samples). */
 	dacstat = inw(dev->iobase + PCI224_DACCON);
