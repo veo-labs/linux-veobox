85d2fe6417a929db2143fb6a25fdf898e5945fa1
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-10-22 15:36:27 -0700

    staging: comedi: das800: use comedi_buf_write_samples()
    Use comedi_buf_write_samples() instead of cfc_write_to_buffer() to add the
    single sample to the async buffer.
    
    The core will add the COMEDI_CB_BLOCK event when data is written to the
    async buffer. Remove the unnecessary event from the driver.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/das800.c
+++ drivers/staging/comedi/drivers/das800.c
@@ -499,13 +499,11 @@ static irqreturn_t das800_interrupt(int irq, void *d)
 		if (s->maxdata == 0x0fff)
 			val >>= 4;	/* 12-bit sample */
 
-		val &= s->maxdata;
-		comedi_buf_write_samples(s, &val, 1);
-
-		if (cmd->stop_src == TRIG_COUNT &&
-		    async->scans_done >= cmd->stop_arg) {
-			async->events |= COMEDI_CB_EOA;
-			break;
+		/* if there are more data points to collect */
+		if (cmd->stop_src == TRIG_NONE || devpriv->count > 0) {
+			val &= s->maxdata;
+			comedi_buf_write_samples(s, &val, 1);
+			devpriv->count--;
 		}
 	}
 
