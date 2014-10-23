c5dfc102f4de5013be9dd593df85b02223e6ebe1
Benjamin Romer <benjamin.romer@unisys.com>
2014-10-23 14:30:18 -0400

    staging: unisys: fix CamelCase names in struct irq_info
    Fix CamelCase names in structure, and references to them:
    
    sendInterruptHandle => send_irq_handle
    recvInterruptHandle => recv_irq_handle
    recvInterruptVector => recv_irq_vector
    recvInterruptShared => recv_irq_shared
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/common-spar/include/channels/controlvmchannel.h
+++ drivers/staging/unisys/common-spar/include/channels/controlvmchannel.h
@@ -127,7 +127,7 @@ struct irq_info {
 	 /**< specifies interrupt handle. It is used to retrieve the
 	  *   corresponding interrupt pin from Monitor; and the
 	  *   interrupt pin is used to connect to the corresponding
-	  *   interrupt.  Used by IOPart-GP only. */
+	  *   intrrupt.  Used by IOPart-GP only. */
 	u64 recv_irq_handle;
 
 	 /**< specifies interrupt vector. It, interrupt pin, and shared are
