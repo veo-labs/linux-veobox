e4277838206c254c431348f3084a52aa5dcbf7c0
Chao Xie <chao.xie@marvell.com>
2014-10-31 10:13:43 +0800

    clk: mmp: add init callback for clk-frac
    For the clk-frac, we need to make sure that the initial
    clock rate is one item of the table.
    If it is not, we use the first item in the table by default.
    
    Signed-off-by: Chao Xie <chao.xie@marvell.com>
    Acked-by: Haojian Zhuang <haojian.zhuang@gmail.com>
    Signed-off-by: Michael Turquette <mturquette@linaro.org>

--- drivers/clk/mmp/clk-frac.c
+++ drivers/clk/mmp/clk-frac.c
@@ -118,7 +118,7 @@ static int clk_factor_set_rate(struct clk_hw *hw, unsigned long drate,
 	return 0;
 }
 
-static void clk_factor_init(struct clk_hw *hw)
+void clk_factor_init(struct clk_hw *hw)
 {
 	struct mmp_clk_factor *factor = to_clk_factor(hw);
 	struct mmp_clk_factor_masks *masks = factor->masks;
