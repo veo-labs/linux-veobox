21cf83d3e739e4b52e309d9db0abddf26055dcca
Nadav Amit <namit@cs.technion.ac.il>
2014-11-02 11:54:56 +0200

    KVM: x86: Inject #GP when loading system segments with non-canonical base
    When emulating LTR/LDTR/LGDT/LIDT, #GP should be injected if the base is
    non-canonical. Otherwise, VM-entry will fail.
    
    Signed-off-by: Nadav Amit <namit@cs.technion.ac.il>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -3388,6 +3388,9 @@ static int em_lgdt_lidt(struct x86_emulate_ctxt *ctxt, bool lgdt)
 			     ctxt->op_bytes);
 	if (rc != X86EMUL_CONTINUE)
 		return rc;
+	if (ctxt->mode == X86EMUL_MODE_PROT64 &&
+	    is_noncanonical_address(desc_ptr.address))
+		return emulate_gp(ctxt, 0);
 	if (lgdt)
 		ctxt->ops->set_gdt(ctxt, &desc_ptr);
 	else
