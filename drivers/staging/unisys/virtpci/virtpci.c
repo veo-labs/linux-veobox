cefd3f866bfcb8617c80f7cb217ef8b60180e6ac
Bryan Thompson <bryan.thompson@unisys.com>
2014-11-03 14:21:03 -0500

    staging: unisys: virtpci: Fix CamelCase vbus driver info variable
    Rename BusDriverInfo variable to bus_driver_info in virtpci.c
    
    Signed-off-by: Bryan Thompson <bryan.thompson@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/virtpci/virtpci.c
+++ drivers/staging/unisys/virtpci/virtpci.c
@@ -149,7 +149,7 @@ static struct virtpci_dev *vpcidev_list_head;
 static DEFINE_RWLOCK(vpcidev_list_lock);
 
 /* filled in with info about this driver, wrt it servicing client busses */
-static struct ultra_vbus_deviceinfo Bus_DriverInfo;
+static struct ultra_vbus_deviceinfo bus_driver_info;
 
 /*****************************************************/
 /* debugfs entries                                   */
@@ -277,7 +277,8 @@ static int add_vbus(struct add_vbus_guestpart *addparams)
 	}
 	write_vbus_chpInfo(vbus->platform_data /* chanptr */ ,
 			   &chipset_driver_info);
-	write_vbus_busInfo(vbus->platform_data /* chanptr */ , &Bus_DriverInfo);
+	write_vbus_busInfo(vbus->platform_data /* chanptr */ ,
+			   &bus_driver_info);
 	LOGINF("Added vbus %d; device %s created successfully\n",
 	       addparams->bus_no, BUS_ID(vbus));
 	POSTCODE_LINUX_2(VPCI_CREATE_EXIT_PC, POSTCODE_SEVERITY_INFO);
@@ -804,7 +805,7 @@ static void fix_vbus_dev_info(struct device *dev, int dev_no, int dev_type,
 	* was previously written by our good counterpart, visorbus.
 	*/
 	write_vbus_chpInfo(pChan, &chipset_driver_info);
-	write_vbus_busInfo(pChan, &Bus_DriverInfo);
+	write_vbus_busInfo(pChan, &bus_driver_info);
 }
 
 /* This function is called to query the existence of a specific device
@@ -1512,7 +1513,7 @@ static int __init virtpci_mod_init(void)
 		return ret;
 	}
 	DBGINF("bus_register successful\n");
-	bus_device_info_init(&Bus_DriverInfo, "clientbus", "virtpci",
+	bus_device_info_init(&bus_driver_info, "clientbus", "virtpci",
 			     VERSION, NULL);
 
 	/* create a root bus used to parent all the virtpci buses. */
