6b913a96f9b15724c0a46b30e5fb274d6878bb57
Damien Lespiau <damien.lespiau@intel.com>
2014-11-04 17:06:48 +0000

    drm/i915/skl: Store the new WM state at the very end of the update
    We're going to add a new step, let's not hide the copy of the new WM
    state inside one inner function, but as a 1st level operation in the WM
    update.
    
    v2: Split the staging results structure to not exceed the 1Kb stack
        allocation in skl_update_wm()
    
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -3452,8 +3452,6 @@ static void skl_write_wm_values(struct drm_i915_private *dev_priv,
 				   new->ddb.cursor[pipe].start);
 		}
 	}
-
-	dev_priv->wm.skl_hw = *new;
 }
 
 static bool skl_update_pipe_wm(struct drm_crtc *crtc,
@@ -3547,6 +3545,9 @@ static void skl_update_wm(struct drm_crtc *crtc)
 
 	skl_update_other_pipe_wm(dev, crtc, &config, results);
 	skl_write_wm_values(dev_priv, results);
+
+	/* store the new configuration */
+	dev_priv->wm.skl_hw = *results;
 }
 
 static void
