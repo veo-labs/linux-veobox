2a69ebee5d4b60e8b711f0e094add14ad45b1aef
Nadav Amit <namit@cs.technion.ac.il>
2014-11-19 17:43:13 +0200

    KVM: x86: Move __linearize masking of la into switch
    In __linearize there is check of the condition whether to check if masking of
    the linear address is needed.  It occurs immediately after switch that
    evaluates the same condition.  Merge them.
    
    Signed-off-by: Nadav Amit <namit@cs.technion.ac.il>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -691,10 +691,9 @@ static __always_inline int __linearize(struct x86_emulate_ctxt *ctxt,
 		*max_size = min_t(u64, ~0u, (u64)lim + 1 - addr.ea);
 		if (size > *max_size)
 			goto bad;
+		la &= (u32)-1;
 		break;
 	}
-	if (ctxt->mode != X86EMUL_MODE_PROT64)
-		la &= (u32)-1;
 	if (insn_aligned(ctxt, size) && ((la & (size - 1)) != 0))
 		return emulate_gp(ctxt, 0);
 	*linear = la;
