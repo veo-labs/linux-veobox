6fccae799d0570278e80505485574ad6c38f5139
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-11-20 15:07:27 -0700

    staging: comedi: cb_pcidas: use subdevice readback for 'trimpot_value'
    Use the comedi_subdevice 'readback' member and the core provided (*insn_read)
    to handle the readback of the write-only trimpot calib subdevice. Remove the
    then unused 'trimpot_value' member from the private data.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/cb_pcidas.c
+++ drivers/staging/comedi/drivers/cb_pcidas.c
@@ -356,7 +356,6 @@ struct cb_pcidas_private {
 	/* divisors of master clock for analog output pacing */
 	unsigned int ao_divisor1;
 	unsigned int ao_divisor2;
-	unsigned int trimpot_value[NUM_CHANNELS_8402];
 	unsigned int dac08_value;
 	unsigned int calibration_source;
 };
@@ -734,20 +733,7 @@ static int cb_pcidas_trimpot_insn_write(struct comedi_device *dev,
 					struct comedi_insn *insn,
 					unsigned int *data)
 {
-	struct cb_pcidas_private *devpriv = dev->private;
 	unsigned int chan = CR_CHAN(insn->chanspec);
-
-	if (insn->n) {
-		unsigned int val = data[insn->n - 1];
-
-		if (devpriv->trimpot_value[chan] != val) {
-			cb_pcidas_trimpot_write(dev, chan, val);
-			devpriv->trimpot_value[chan] = val;
-		}
-	}
-
-	return insn->n;
-}
 
 	if (insn->n) {
 		unsigned int val = data[insn->n - 1];
@@ -1517,11 +1503,15 @@ static int cb_pcidas_auto_attach(struct comedi_device *dev,
 		s->n_chan = NUM_CHANNELS_8402;
 		s->maxdata = 0xff;
 	}
-	s->insn_read = trimpot_read_insn;
 	s->insn_write = cb_pcidas_trimpot_insn_write;
+
+	ret = comedi_alloc_subdev_readback(s);
+	if (ret)
+		return ret;
+
 	for (i = 0; i < s->n_chan; i++) {
 		cb_pcidas_trimpot_write(dev, i, s->maxdata / 2);
-		devpriv->trimpot_value[i] = s->maxdata / 2;
+		s->readback[i] = s->maxdata / 2;
 	}
 
 	/*  dac08 caldac */
