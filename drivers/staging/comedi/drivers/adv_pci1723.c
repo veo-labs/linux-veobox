c33902c1344d63f21ed773f60496d663d9baed87
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-11-21 14:22:27 -0700

    staging: comedi: adv_pci1723: tidy up register map
    For aesthetics, rename the defines used for the register map offsets and
    remove the unnecessary comments. Add the bit defines for the registers.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/adv_pci1723.c
+++ drivers/staging/comedi/drivers/adv_pci1723.c
@@ -82,6 +82,48 @@
 #define PCI1723_VREF_0V			(1 << 0)
 #define PCI1723_VREF_POS10V		(3 << 0)
 
+struct pci1723_private {
+	unsigned char da_range[8];	/* D/A output range for each channel */
+	unsigned short ao_data[8];	/* data output buffer */
+};
+
+/*
+ * PCI Bar 2 I/O Register map (dev->iobase)
+ */
+#define PCI1723_AO_REG(x)		(0x00 + ((x) * 2))
+#define PCI1723_BOARD_ID_REG		0x10
+#define PCI1723_BOARD_ID_MASK		(0xf << 0)
+#define PCI1723_SYNC_CTRL_REG		0x12
+#define PCI1723_SYNC_CTRL_ASYNC		(0 << 0)
+#define PCI1723_SYNC_CTRL_SYNC		(1 << 0)
+#define PCI1723_CTRL_REG		0x14
+#define PCI1723_CTRL_BUSY		(1 << 15)
+#define PCI1723_CTRL_INIT		(1 << 14)
+#define PCI1723_CTRL_SELF		(1 << 8)
+#define PCI1723_CTRL_IDX(x)		(((x) & 0x3) << 6)
+#define PCI1723_CTRL_RANGE(x)		(((x) & 0x3) << 4)
+#define PCI1723_CTRL_GAIN		(0 << 3)
+#define PCI1723_CTRL_OFFSET		(1 << 3)
+#define PCI1723_CTRL_CHAN(x)		(((x) & 0x7) << 0)
+#define PCI1723_CALIB_CTRL_REG		0x16
+#define PCI1723_CALIB_CTRL_CS		(1 << 2)
+#define PCI1723_CALIB_CTRL_DAT		(1 << 1)
+#define PCI1723_CALIB_CTRL_CLK		(1 << 0)
+#define PCI1723_CALIB_STROBE_REG	0x18
+#define PCI1723_DIO_CTRL_REG		0x1a
+#define PCI1723_DIO_CTRL_HDIO		(1 << 1)
+#define PCI1723_DIO_CTRL_LDIO		(1 << 0)
+#define PCI1723_DIO_DATA_REG		0x1c
+#define PCI1723_CALIB_DATA_REG		0x1e
+#define PCI1723_SYNC_STROBE_REG		0x20
+#define PCI1723_RESET_AO_STROBE_REG	0x22
+#define PCI1723_RESET_CALIB_STROBE_REG	0x24
+#define PCI1723_RANGE_STROBE_REG	0x26
+#define PCI1723_VREF_REG		0x28
+#define PCI1723_VREF_NEG10V		(0 << 0)
+#define PCI1723_VREF_0V			(1 << 0)
+#define PCI1723_VREF_POS10V		(3 << 0)
+
 static int pci1723_ao_insn_write(struct comedi_device *dev,
 				 struct comedi_subdevice *s,
 				 struct comedi_insn *insn,
@@ -90,11 +132,54 @@ static int pci1723_ao_insn_write(struct comedi_device *dev,
 	unsigned int chan = CR_CHAN(insn->chanspec);
 	int i;
 
-	for (i = 0; i < insn->n; i++) {
-		unsigned int val = data[i];
+	outw(PCI1723_SYNC_CTRL_SYNC, dev->iobase + PCI1723_SYNC_CTRL_REG);
 
-		outw(val, dev->iobase + PCI1723_AO_REG(chan));
-		s->readback[chan] = val;
+	for (i = 0; i < 8; i++) {
+		/* set all outputs to 0V */
+		devpriv->ao_data[i] = 0x8000;
+		outw(devpriv->ao_data[i], dev->iobase + PCI1723_AO_REG(i));
+		/* set all ranges to +/- 10V */
+		devpriv->da_range[i] = 0;
+		outw(((devpriv->da_range[i] << 4) | i),
+		     PCI1723_CTRL_REG);
+	}
+
+	outw(0, dev->iobase + PCI1723_RANGE_STROBE_REG);
+	outw(0, dev->iobase + PCI1723_SYNC_STROBE_REG);
+
+	outw(PCI1723_SYNC_CTRL_ASYNC, dev->iobase + PCI1723_SYNC_CTRL_REG);
+
+	return 0;
+}
+
+static int pci1723_insn_read_ao(struct comedi_device *dev,
+				struct comedi_subdevice *s,
+				struct comedi_insn *insn, unsigned int *data)
+{
+	struct pci1723_private *devpriv = dev->private;
+	int n, chan;
+
+	chan = CR_CHAN(insn->chanspec);
+	for (n = 0; n < insn->n; n++)
+		data[n] = devpriv->ao_data[chan];
+
+	return n;
+}
+
+/*
+  analog data output;
+*/
+static int pci1723_ao_write_winsn(struct comedi_device *dev,
+				  struct comedi_subdevice *s,
+				  struct comedi_insn *insn, unsigned int *data)
+{
+	struct pci1723_private *devpriv = dev->private;
+	unsigned int chan = CR_CHAN(insn->chanspec);
+	int n;
+
+	for (n = 0; n < insn->n; n++) {
+		devpriv->ao_data[chan] = data[n];
+		outw(data[n], dev->iobase + PCI1723_AO_REG(chan));
 	}
 
 	return insn->n;
@@ -117,7 +202,7 @@ static int pci1723_dio_insn_config(struct comedi_device *dev,
 	if (!(s->io_bits & 0x00ff))
 		mode |= PCI1723_DIO_CTRL_LDIO;	/* low byte input */
 	if (!(s->io_bits & 0xff00))
-		mode |= PCI1723_DIO_CTRL_HDIO;	/* high byte input */
+		mode |= 0x0002;		/* high byte input */
 	outw(mode, dev->iobase + PCI1723_DIO_CTRL_REG);
 
 	return insn->n;
@@ -190,13 +275,25 @@ static int pci1723_auto_attach(struct comedi_device *dev,
 	s->insn_config	= pci1723_dio_insn_config;
 	s->insn_bits	= pci1723_dio_insn_bits;
 
-	/* get initial DIO direction and state */
-	val = inw(dev->iobase + PCI1723_DIO_CTRL_REG);
-	if (!(val & PCI1723_DIO_CTRL_LDIO))
-		s->io_bits |= 0x00ff;	/* low byte output */
-	if (!(val & PCI1723_DIO_CTRL_HDIO))
-		s->io_bits |= 0xff00;	/* high byte output */
+	/* read DIO config */
+	switch (inw(dev->iobase + PCI1723_DIO_CTRL_REG) & 0x03) {
+	case 0x00:	/* low byte output, high byte output */
+		s->io_bits = 0xFFFF;
+		break;
+	case 0x01:	/* low byte input, high byte output */
+		s->io_bits = 0xFF00;
+		break;
+	case 0x02:	/* low byte output, high byte input */
+		s->io_bits = 0x00FF;
+		break;
+	case 0x03:	/* low byte input, high byte input */
+		s->io_bits = 0x0000;
+		break;
+	}
+	/* read DIO port state */
 	s->state = inw(dev->iobase + PCI1723_DIO_DATA_REG);
+
+	pci1723_reset(dev);
 
 	return 0;
 }
