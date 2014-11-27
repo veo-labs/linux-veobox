0d8cc961d2d6b6c9e5b60cd35650e334263d0356
Radim Krčmář <rkrcmar@redhat.com>
2014-11-27 20:03:13 +0100

    KVM: x86: allow 256 logical x2APICs again
    While fixing an x2apic bug,
     17d68b7 KVM: x86: fix guest-initiated crash with x2apic (CVE-2013-6376)
    we've made only one cluster available.  This means that the amount of
    logically addressible x2APICs was reduced to 16 and VCPUs kept
    overwriting themselves in that region, so even the first cluster wasn't
    set up correctly.
    
    This patch extends x2APIC support back to the logical_map's limit, and
    keeps the CVE fixed as messages for non-present APICs are dropped.
    
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -162,8 +162,7 @@ static void recalculate_apic_map(struct kvm *kvm)
 		if (apic_x2apic_mode(apic)) {
 			new->ldr_bits = 32;
 			new->cid_shift = 16;
-			new->cid_mask = (1 << KVM_X2APIC_CID_BITS) - 1;
-			new->lid_mask = 0xffff;
+			new->cid_mask = new->lid_mask = 0xffff;
 			new->broadcast = X2APIC_BROADCAST;
 		} else if (kvm_apic_get_reg(apic, APIC_LDR)) {
 			if (kvm_apic_get_reg(apic, APIC_DFR) ==
