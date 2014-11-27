4df87380687054e785c8c30e8048357b9aa28af7
Radim Krčmář <rkrcmar@redhat.com>
2014-11-27 20:03:11 +0100

    KVM: x86: deliver phys lowest-prio
    Physical mode can't address more than one APIC, but lowest-prio is
    allowed, so we just reuse our paths.
    
    SDM 10.6.2.1 Physical Destination:
      Also, for any non-broadcast IPI or I/O subsystem initiated interrupt
      with lowest priority delivery mode, software must ensure that APICs
      defined in the interrupt address are present and enabled to receive
      interrupts.
    
    We could warn on top of that.
    
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -693,10 +693,7 @@ bool kvm_irq_delivery_to_apic_fast(struct kvm *kvm, struct kvm_lapic *src,
 	ret = true;
 
 	if (irq->dest_mode == 0) { /* physical mode */
-		if (irq->delivery_mode == APIC_DM_LOWEST)
-			goto out;
-
-		dst = &map->phys_map[irq->dest_id];
+		dst = &map->phys_map[irq->dest_id & 0xff];
 	} else {
 		u32 mda = irq->dest_id << (32 - map->ldr_bits);
 		u16 cid = apic_cluster_id(map, mda);
