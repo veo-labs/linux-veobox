851b9010e6b7adc6d5dc68a8585870600576a365
Anjana Sasindran <anjanasasindran123@gmail.com>
2014-11-29 20:38:45 +0530

    staging:ft1000_pcmcia:Added a blank line after declaration
    This patch fix the checkpatch.pl warning
    
    WARNING: Missing a blank line after declaration
    
    Signed-off-by: Anjana Sasindran <anjanasasindran123@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ft1000/ft1000-pcmcia/ft1000_hw.c
+++ drivers/staging/ft1000/ft1000-pcmcia/ft1000_hw.c
@@ -2033,6 +2033,7 @@ static void ft1000_get_drvinfo(struct net_device *dev,
 static u32 ft1000_get_link(struct net_device *dev)
 {
 	struct ft1000_info *info;
+
 	info = netdev_priv(dev);
 	return info->mediastate;
 }
