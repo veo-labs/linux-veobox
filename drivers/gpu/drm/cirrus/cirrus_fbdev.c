b4537c1ef41619cbcc8d4f242f1fe857ea11a901
Thierry Reding <treding@nvidia.com>
2014-12-17 16:39:59 +0100

    drm/fb-helper: Propagate errors from initial config failure
    Make drm_fb_helper_initial_config() return an int rather than a bool so
    that the error can be properly propagated. While at it, update drivers
    to propagate errors further rather than just ignore them.
    
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
    Cc: Rob Clark <robdclark@gmail.com>
    Cc: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: Christian König <christian.koenig@amd.com>
    Cc: Ben Skeggs <bskeggs@redhat.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/cirrus/cirrus_fbdev.c
+++ drivers/gpu/drm/cirrus/cirrus_fbdev.c
@@ -318,16 +318,26 @@ int cirrus_fbdev_init(struct cirrus_device *cdev)
 	ret = drm_fb_helper_init(cdev->dev, &gfbdev->helper,
 				 cdev->num_crtc, CIRRUSFB_CONN_LIMIT);
 	if (ret)
-		return ret;
+		goto free;
 
 	ret = drm_fb_helper_single_add_all_connectors(&gfbdev->helper);
 	if (ret)
-		return ret;
+		goto fini;
 
 	/* disable all the possible outputs/crtcs before entering KMS mode */
 	drm_helper_disable_unused_functions(cdev->dev);
 
-	return drm_fb_helper_initial_config(&gfbdev->helper, bpp_sel);
+	ret = drm_fb_helper_initial_config(&gfbdev->helper, bpp_sel);
+	if (ret)
+		goto fini;
+
+	return 0;
+
+free:
+	kfree(gfbdev);
+fini:
+	drm_fb_helper_fini(&gfbdev->helper);
+	return ret;
 }
 
 void cirrus_fbdev_fini(struct cirrus_device *cdev)
